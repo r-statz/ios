5c1049ae6f6d46046abbbe3166e7371a7a90da88
rstatz <robinstatz@gmail.com>
2018-09-05 14:37:23 -0600

    api call showing up on simulator:

---
+++ fakerfact/Footer.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+const Footer = () => {
+
+  return(
+    <View>
+      <Text>
+        Hello world
+      </Text>
+    </View>
+  )
+}
+
+export default Footer
