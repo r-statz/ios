39c1d0288d2a5d99b8f8f9d1602f1d54380afdc2
rstatz <robinstatz@gmail.com>
2018-09-05 13:09:44 -0600

    rm -rfed again

--- fakerfact/store.js
+++
@@ -1,8 +0,0 @@
-import thunkMiddleware from 'redux-thunk'
-import logger from 'redux-logger'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import main from './Reducers/main'
-import rootReducer from './Reducers/reducer'
-
-
-export default () => createStore(rootReducer, applyMiddleware(thunkMiddleware))
