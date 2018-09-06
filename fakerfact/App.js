4c193e53b23662396114d576baa297d2e3498bfe
rstatz <robinstatz@gmail.com>
2018-09-06 10:56:04 -0600

    dealt with hateoas address. Got the url linked to walt. fine tuning.

--- fakerfact/App.js
+++ fakerfact/App.js
@@ -1,31 +1,48 @@
 import React, {Component} from 'react'
-import {Platform, StyleSheet, Text, View, Button, TextInput} from 'react-native'
+import {Platform, StyleSheet, Text, View, Button, TextInput, Linking} from 'react-native'
 import Footer from './Footer'
 import Header from './Header'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-})
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// })
 
 // type Props = {}
 export default class App extends Component<Props> {
   state = {
-    text: '',
-    json: {}
+    inputUrl: '',
+    predictions: '',
+    results: {}
   }
 
   componentDidMount = async() => {
     const response = await fetch('https://api.fakerfact.org/api')
     const json = await response.json()
-    this.setState({json})
+    this.setState({predictions: json._links.predictions.href})
+  }
+
+  async postUrl(url) {
+    console.log(this.state.newsUrl, 'url')
+    const response = await fetch(this.state.predictions, {
+      method: 'POST',
+      body: JSON.stringify({ 'url': url}),
+      headers: {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json'
+        }
+    })
+    const json = await response.json()
+    this.setState({results: json})
 
   }
 
-  checkUrl = () => {
-    console.log('state', this.state.text)
+  checkUrl = (url) => {
+    // console.log(url, 'url')
+    this.postUrl(url)
+    // console.log('state', this.state)
   }
 
   render() {
@@ -36,16 +53,20 @@ export default class App extends Component<Props> {
           <TextInput
             id='id'
             style={{height: 40, borderColor: 'pink', borderWidth: 1}}
-            onChangeText={(e) => this.setState({text: e})}
+            onChangeText={(e) => this.setState({inputUrl: e})}
             // value={this.props.state.text}
           />
           <Button
             title='Check'
             style={{height: 40, borderColor: 'pink', borderWidth: 1}}
-            onPress={() => { this.checkUrl() }}
+            onPress={() => { this.checkUrl(this.state.inputUrl) }}
           />
         </View>
-        <Text style={styles.welcome}>{JSON.stringify(this.state.json)}</Text>
+        <Text style={styles.welcome}>{JSON.stringify(this.state.results)}</Text>
+        <Text style={{color: 'blue'}}
+      onPress={() => Linking.openURL('https://google.com')}>
+  Google
+</Text>
         <Footer />
       </View>
     )
