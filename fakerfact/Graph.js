e894bb6f477c7a407f3ef14db8256e640eaf8095
rstatz <robinstatz@gmail.com>
2018-09-11 15:25:01 -0600

    getting the graph online.

--- fakerfact/Graph.js
+++ fakerfact/Graph.js
@@ -1,41 +1,22 @@
-import React, {Animated, Component} from 'react-native'
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+import Styles from './Styles'
 
-// class Graph extends Component {
-//   constructor () {
-//     super()
-//     const width = {pts: 30, ast:...}
-//     this.state = {
-//       pts: new Animated.Value(width.pts)
-//       ast: new Animated.Value(width.ast),
-//       reb: new Animated.Value(width.reb)
-//     }
-//   }
-//
-//   handeleAnimation () {
-//     const timing = Animated.timing
-//     const width = {pts: 14, ast:...}
-//     const indicators = ['pts', 'ast', 'reb']
-//     Animated.parallel(indicators.map(item => {
-//       return timing(this.state[item], {toValue: width[item]})
-//     })).start()
-//   }
-//
-//   render () {
-//    const {pts, ast, reb, stl, blk, tov, min} = this.state
-//
-//    return (
-//       <View>
-//        {pts &&
-//           <Animated.View style={[styles.bar, styles.points, {width: pts}]} />
-//         }
-//         {ast &&
-//           <Animated.View style={[styles.bar, styles.assists, {width: ast}]} />
-//         }
-//         {reb &&
-//           <Animated.View style={[styles.bar, styles.rebounds, {width: reb}]} />
-//         }
-//         <Text onPress={this.handeleAnimation.bind(this)}>Button</Text>
-//       </View>
-//    )
-//   }
-// }
+
+const Graph = (props) => {
+  // console.log(name, color, value, 'name')
+
+
+  return (
+    <View style={{flex: 1, flexDirection: 'row', alignItems: 'stretch'}}>
+      <View style={{flex: 1, height: 25, alignItems: 'flex-end'}}>
+        <Text>{props.x.name}</Text>
+      </View>
+      <View style={{flex: 1, height: 25, backgroundColor: 'skyblue'}} />
+      <View style={{flex: 1, height: 25, backgroundColor: 'steelblue'}} />
+    </View>
+  )
+
+}
+
+export default Graph
