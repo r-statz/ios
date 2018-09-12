96fddeb98cc9a6b01fcc7a37e5a7873193d430ee
rstatz <robinstatz@gmail.com>
2018-09-12 09:38:13 -0600

    graph is functionalgit add -A! And there is no hate.

--- fakerfact/Graphs.js
+++ fakerfact/Graphs.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react'
 import {AppRegistry, Text, View} from 'react-native'
 import Styles from './Styles'
+import Color from 'color'
 // import Graph from './Graph'
 
 const Graphs = ({predictions}) => {
 
-let bars = predictions.map((x, i) =>
+let colorArray = predictions.map(x => Color(x.color).fade(0.8).rgb().string())
+
+let noHate = predictions.slice(0, predictions.length-1)
+
+let bars = noHate.map((x, i) =>
 <View key={i} style={Styles.graphBox}>
   <View style={Styles.barBox}>
     <View style={{flex: 1, flexDirection: 'column'}}>
@@ -15,9 +20,9 @@ let bars = predictions.map((x, i) =>
         <Text style={Styles.barText}>{ x.name }</Text>
         </View>
 
-        <View style={{flex: x.value, height: 25, backgroundColor: 'pink'}} />
+        <View style={{flex: Number(x.value) * Number(2), height: 25, backgroundColor: x.color}} />
 
-        <View style={{flex: (Number(2) - Number(x.value)), height: 25, backgroundColor: 'steelblue'}} />
+        <View style={{flex: (Number(2) - Number(x.value * 2)) , height: 25, backgroundColor: colorArray[i]}} />
 
       </View>
     </View>
