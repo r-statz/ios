d1be7f1f447f3605088a085c56ac29fb571f3017
rstatz <robinstatz@gmail.com>
2018-09-12 14:41:43 -0600

    fixed screen so that if no url is entered the gray box with 'checking url' does not pop up. Figured out that implementing scrollView means a double click on the simulator screen, but on the demo phone version, the first screen tap makes the keyboard disappear.

--- fakerfact/App.js
+++ fakerfact/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-import {Text, View, Button, TextInput, Linking, ScrollView} from 'react-native'
+import {Text, View, Button, TextInput, Linking} from 'react-native'
 import Styles from './Styles'
 import Footer from './Footer'
 import Header from './Header'
 import Body from './Body'
+
 
 console.disableYellowBox = true;
 
@@ -23,6 +24,7 @@ export default class App extends Component {
   }
 
   async postUrl(url) {
+    try {
     const response = await fetch(this.state.apiUrl, {
       method: 'POST',
       body: JSON.stringify({ 'url': url}),
@@ -33,13 +35,17 @@ export default class App extends Component {
     })
     const json = await response.json()
     this.setState({results: json})
+  } catch(err) {
+    console.log(err, "yo mammy")
   }
+}
 
   logoButton = () => {
     this.setState({toggle: false})
   }
 
   inputText = (e) => {
+    console.log(e, 'e')
     this.setState({inputUrl:  e})
   }
 
@@ -49,10 +55,11 @@ export default class App extends Component {
                   placeHolder: 'Enter another URL',
                   inputUrl: ''
                 })
-                console.log('is clicked')
   }
 
   render() {
+    console.log(this.state.results.predictions)
+    // console.log(this.state.inputURl, 'inputUrl')
     return (
 
       <View style={Styles.container}>
@@ -60,10 +67,9 @@ export default class App extends Component {
           logoButton = {this.logoButton}
           toggle={this.state.toggle}
         />
-
         <Body
-          // style={Styles.body}
-          postUrl={this.postUrl} checkUrl={this.checkUrl}
+          postUrl={this.postUrl}
+          checkUrl={this.checkUrl}
           walt={ this.state.results.walt_says}
           linkingUrl = {this.state.results.url}
           predictions={this.state.results.predictions}
@@ -71,12 +77,10 @@ export default class App extends Component {
           placeHolder={this.state.placeHolder}
           inputText={this.inputText}
           inputUrl={this.state.inputUrl}
-          logoButton={this.logoButton}
+          // logoButton={this.logoButton}
         />
-
         <Footer />
       </View>
-
     )
   }
 }
--- fakerfact/Body.js
+++ fakerfact/Body.js
@@ -3,27 +3,27 @@ import {Text, TextInput, View, Linking, ScrollView} from 'react-native'
 import Button from 'react-native-button'
 import Styles from './Styles'
 import Graphs from './Graphs'
+// import Error from './Error'
 
 export default class Body extends Component {
 
   render() {
     const { predictions, linkingUrl, walt } = this.props
 
-    console.log(this.props.toggle);
     let display = this.props.toggle ?
       <View>
-        <View style={Styles.checkingUrl}>
-          <Text style={Styles.checkingUrlText}>Checking URL:</Text>
+        <View style={ Styles.checkingUrl }>
+          <Text style={ Styles.checkingUrlText }>Checking URL:</Text>
           <Text style={{color: 'blue'}}
             onPress={() => Linking.openURL(linkingUrl)}>
           { linkingUrl }
           </Text>
           </View>
         <View >
-          <Text style={Styles.waltSays}>{ walt}</Text>
+          <Text style={ Styles.waltSays }>{ walt }</Text>
         </View>
         <View>
-          { predictions ? <Graphs predictions={ predictions } /> : null }
+          { predictions ? <Graphs predictions={ predictions } /> : null  }
           </View>
       </View> : <View></View>
 
@@ -40,10 +40,10 @@ export default class Body extends Component {
         />
           <View style={Styles.checkBox}>
             <Button
-              containerStyle={Styles.buttonBox}
+              containerStyle={ Styles.buttonBox }
               title='Check'
               style={Styles.button}
-              onPress={ this.props.checkUrl }
+              onPress={ !this.props.inputUrl.length ? null : this.props.checkUrl }
               >Check
             </Button>
           </View>
